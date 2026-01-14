9920e5ce9daa83c72ac9ba9df679b54f51598788
Eloon Ing <eing067@aucklanduni.ac.nz>
2026-01-14 15:54:56 +1300

    Added icon section as well as images.

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,4 +1,7 @@
 import Navbar from './navbar'
+import locationIcon from '../assets/location.png';
+import wholesaleIcon from '../assets/wholesale.png';
+import checkIcon from '../assets/check.png';
 import mainImage from '../assets/main.png';
 
 function Home() {
@@ -7,10 +10,35 @@ function Home() {
     <>
        <Navbar/> 
       <div className="w-full bg-black shadow-sm top-0 left-0 right-0 h-[80vh] relative">
-        <img src={mainImage} alt="Main Banner" className="w-full h-full object-cover oberabsolute z-1"/>
+        <img src={mainImage} alt="Main Banner" className="w-full h-full object-cover absolute z-1"/>
         <h1 className="absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-4xl md:text-6xl font-bold text-center">
           Your Trusted Source of Asian Goods in New Zealand
         </h1>
+      </div>
+      {/* Icon Section */}
+      <div className="w-full bg-white h-[10vh] flex items-center justify-center">
+        <div className="w-100 h-20 flex items-center justify-between">
+          <div className="w-12 h-12 rounded-full flex items-center justify-center">
+              <img src={locationIcon} alt="Location icon" className="object-contain"/>
+          </div>
+          <h2 className="text-black text-2xl font-semibold p-4 text-center">Two Convenient Locations</h2>
+          <div className="border-l border-gray-300 h-full"></div>
+        </div>
+
+        <div className="w-100 h-20 flex items-center justify-between ml-10">
+          <div className="w-12 h-12 rounded-full flex items-center justify-center">
+              <img src={wholesaleIcon} alt="Wholesale icon" className="object-contain"/>
+          </div>
+          <h2 className="text-black text-2xl font-semibold p-4 text-center">Wholesale & Retail Options</h2>
+          <div className="border-l border-gray-300 h-full"></div>
+        </div>
+
+        <div className="w-80 h-20 flex items-center justify-between ml-10">
+          <div className="w-12 h-12 rounded-full flex items-center justify-center">
+              <img src={checkIcon} alt="Check icon" className="object-contain"/>
+          </div>
+          <h2 className="text-black text-2xl font-semibold p-4 text-center">Quality Guaranteed</h2>
+        </div>
       </div>
     </>
   )
