cd5e080720dcea995ec71b5e234d8b0438633dee
Eloon Ing <eing067@aucklanduni.ac.nz>
2026-01-16 15:11:45 +1300

    Added embedded google maps, and fixed Staurday Auckland open times.

--- src/components/stores.jsx
+++ src/components/stores.jsx
@@ -16,6 +16,14 @@ function Stores() {
         if (day === 0) {
             setAucklandState(0);
             setHamiltonState(0);
+        } else if (day === 6) {
+             if (hour >= 10 && hour < 16) {
+                setAucklandState(1);
+            } else if (hour >= 16 && hour < 17) {
+                setAucklandState(2);
+            } else {
+                setAucklandState(0);
+            }
         } else {
             // Auckland Store
             if (hour >= 9 && hour < 16) {
@@ -94,13 +102,20 @@ function Stores() {
                     </div>
                     <div className="flex justify-between">
                         <span className="font-semibold text-black">Saturday</span>
-                        <span className="text-black">9:00am - 5:00pm</span>
+                        <span className="text-black">10:00am - 5:00pm</span>
                     </div>
                     </div>
                     
                     {/* Map placeholder */}
-                    <div className="w-full h-48 bg-gray-200 rounded mb-4 flex items-center justify-center">
-                        <span className="text-gray-500">Map View</span>
+                    <div className="w-full h-100 bg-gray-200 rounded mb-4 flex items-center justify-center">
+                        <iframe 
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6377.716950796189!2d174.83579619531918!3d-36.94154901302845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d0d4eb9825f5013%3A0x17aa10ae5f4183d9!2sIngs%20Trading%20Ltd!5e0!3m2!1sen!2snz!4v1768529338123!5m2!1sen!2snz" 
+                            className="cover-full w-full h-full rounded mb-4"
+                            style={{ border: 0 }} 
+                            allowFullScreen="" 
+                            loading="lazy" 
+                            referrerPolicy="no-referrer-when-downgrade"
+                        />
                     </div>
                     
                     {/* Status and Phone */}
@@ -151,8 +166,15 @@ function Stores() {
                     </div>
                     
                     {/* Map placeholder */}
-                    <div className="w-full h-48 bg-gray-200 rounded mb-4 flex items-center justify-center">
-                    <span className="text-gray-500">Map View</span>
+                    <div className="w-full h-100 bg-gray-200 rounded mb-4 flex items-center justify-center">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6306.260393118721!2d175.26593904391362!3d-37.78698845646589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d6d220f606ea8e1%3A0x9d97216f18026327!2sIngs%20Trading!5e0!3m2!1sen!2snz!4v1768527965010!5m2!1sen!2snz" 
+                        className="cover-full w-full h-full rounded mb-4"
+                        style={{ border: 0 }} 
+                        allowFullScreen="" 
+                        loading="lazy" 
+                        referrerPolicy="no-referrer-when-downgrade"
+                    />
                     </div>
                     
                     {/* Status and Phone */}
